bfd3d5c0a52634541bd1f29775e132cb0bf88454
Florin Popa <flopaconsult@gmail.com>
2013-04-19 13:27:55 +0300

    Moved zzz_sudo to Berkshelf

---
+++ attributes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: sudo
+# Attribute File:: sudoers
+#
+
+if default[:authorization][:sudo][:groups].nil? || default[:authorization][:sudo][:groups].empty?
+  default[:authorization][:sudo][:groups] = Array.new 
+end
+if default[:authorization][:sudo][:users].nil? || default[:authorization][:sudo][:users].empty?
+  default[:authorization][:sudo][:users] = Array.new
+end
+
+default[:authorization][:sudo][:users] << "ubuntu"
+#default[:authorization][:sudo][:users] << "fdrescher"
+default[:authorization][:sudo][:groups] << "sysadmin"
