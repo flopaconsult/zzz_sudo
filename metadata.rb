da1f72e39ec5200d6da2f2a6a7e6739e1c6b4ac0
Florin Popa <flopaconsult@gmail.com>
2013-04-19 13:37:24 +0300

    changes from florin-wrk on Fri Apr 19 13:37:24 EEST 2013

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ license          "All rights reserved"
 description      "Installs/Configures sudo"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
-depends 'sudo','~> 1.0.0'
+depends 'sudo','= 1.0.0'
